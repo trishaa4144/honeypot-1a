ab59264cc843a0f6332acdd2211178406b1fdb35
Trisha <77016955+trishaa4144@users.noreply.github.com>
2023-10-17 14:24:51 -0400

    update flush_iptables.sh

--- dev_scripts/flush_iptables.sh
+++ dev_scripts/flush_iptables.sh
@@ -4,6 +4,7 @@ sudo iptables -F # flush all chains
 sudo iptables -t nat -F
 sudo iptables -t mangle -F
 sudo iptables -X # delete all chains
+sudo reboot
 
 # Credit:
 # https://serverfault.com/questions/166908/iptables-command-to-clear-all-existing-rules
