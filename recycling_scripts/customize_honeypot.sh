9f286f95fe8b0e135c613000c99381dfe42be5ca
Trisha <77016955+trishaa4144@users.noreply.github.com>
2023-10-19 04:24:11 -0400

    fix directory

--- recycling_scripts/customize_honeypot.sh
+++ recycling_scripts/customize_honeypot.sh
@@ -17,7 +17,7 @@ echo $language
 python3 /home/student/honeypot-1a/honeymaker.py --language $language
 
 # Add functionality to copy generated files/folders from "generated" folder to the container.
-sudo cp -r "/home/student/generated/"* /var/lib/lxc/$container_name/rootfs/root/home
+sudo cp -r "/home/student/generated/"* /var/lib/lxc/$container_name/rootfs/home
 
 # TODO
 # Set up to run a script on shell start that will copy the files from home to the home
