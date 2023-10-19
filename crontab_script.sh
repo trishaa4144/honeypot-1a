03057db5b7d50a7a11557afbaeda749695c80471
Neha Anumalasetty <67026251+nla013neha@users.noreply.github.com>
2023-10-19 03:37:12 -0400

    updated crontab script to have correct file paths to run scripts

--- crontab_script.sh
+++ crontab_script.sh
@@ -1,4 +1,4 @@
-*/5 * * * * ~/honeypot-1a/recycling_scripts/check_container.sh >> ~/check_logs/check_container_$(date --iso-8601=seconds).log
-@reboot ~/honeypot-1a/recycling_scripts/check_container.sh
-@reboot ~/honeypot-1a/recycling_scripts/firewall_rules.sh
-*/15 * * * * ~/honeypot-1a/recycling_scripts/monitor_all.sh >> ~/monitor_logs/monitor_$(date --iso-8601=seconds).log
+*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> ~/check_logs/check_container_$(date --iso-8601=seconds).log
+@reboot /home/student/honeypot-1a/recycling_scripts/check_container.sh
+@reboot /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
+*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> ~/monitor_logs/monitor_$(date --iso-8601=seconds).log
