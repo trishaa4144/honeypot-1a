c2b826df50e96b0caa7cc975c9c9ef524039f5a4
Neha Anumalasetty <67026251+nla013neha@users.noreply.github.com>
2023-10-19 03:48:04 -0400

    added comment for crontab script

--- crontab_script.sh
+++ crontab_script.sh
@@ -1,3 +1,10 @@
+# This crontab script will run the check_container.sh script every five minutes. It will also run the
+# check_container.sh script to ensure all honeypot containers will automatically restart after a full
+# reboot of the host. Additionally, we also called the firewall_rules.sh script to be called at every 
+# reboot to make sure our baseline firewall rules will be automatically re-applied after full reboot 
+# of the host. Lastly, we will run the monitor_all.sh script every 15 minutes to monitor the states of 
+# our honeypots.  
+
 */5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> ~/check_logs/check_container_$(date --iso-8601=seconds).log
 @reboot /home/student/honeypot-1a/recycling_scripts/check_container.sh
 @reboot /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
