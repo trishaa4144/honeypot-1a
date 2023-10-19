d2bdd0a941715034b9b0d8acf89f2425fd0fe26b
Neha Anumalasetty <67026251+nla013neha@users.noreply.github.com>
2023-10-19 18:15:32 -0400

    crontab update

--- crontab_script.sh
+++ crontab_script.sh
@@ -5,7 +5,8 @@
 # of the host. Lastly, we will run the monitor_all.sh script every 15 minutes to monitor the states of 
 # our honeypots.  
 
-*/25 * * * * echo "$(/home/student/honeypot-1a/recycling_scripts/check_container.sh)" >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log
+*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log
+@reboot sh /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
 @reboot /home/student/honeypot-1a/recycling_scripts/check_container.sh
-@reboot /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
-*/15 * * * * echo "$(/home/student/honeypot-1a/recycling_scripts/monitor_all.sh)" >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log
+@reboot systemctl start netdata
+*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log
