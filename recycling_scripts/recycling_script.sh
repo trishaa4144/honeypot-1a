0d3db4b38ecb076d38d0b05ca2fab4d5342fab8e
julzhou121 <145222988+julzhou121@users.noreply.github.com>
2023-10-19 17:57:58 -0400

    added temporary thing to echo the variable

--- recycling_scripts/recycling_script.sh
+++ recycling_scripts/recycling_script.sh
@@ -69,6 +69,10 @@ if [[ -e /home/student/hpotinfo/time_$container_name ]]; then
 
     # Done this way because after the first instance is done, the container after that to be deleted will be shifted up to uid 0,
     # so on and so forth
+    
+    # DELETE LATER
+    echo $(sudo forever list | grep -w $container_name | cut -d " " -f5 | sed 's/[][]//g')
+
     sudo forever stop $(sudo forever list | grep -w $container_name | cut -d " " -f5 | sed 's/[][]//g')
     
     # Deletes the container entirely as it is ready to be recycled
