475dc799889933f474a5bb0bffe911dda9d446b2
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-20 02:08:00 -0400

    Edit reboot script

--- reboot_script.sh
+++ reboot_script.sh
@@ -4,7 +4,4 @@ systemctl start netdata
 sleep 5
 /home/student/honeypot-1a/recycling_scripts/check_container.sh
 sleep 5
-(sudo crontab -l ; echo "*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log")| sudo crontab -
-(sudo crontab -l ; echo "*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log")| sudo crontab -
-
 touch /tmp/reboot_flag
