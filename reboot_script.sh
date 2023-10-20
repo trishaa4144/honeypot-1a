e1336c44d4de6931d472c82bc1773cab14399d79
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-20 01:01:48 -0400

    Append crontab for monitor

--- reboot_script.sh
+++ reboot_script.sh
@@ -5,3 +5,4 @@ sleep 5
 /home/student/honeypot-1a/recycling_scripts/check_container.sh
 sleep 5
 (sudo crontab -l ; echo "*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log")| sudo crontab -
+(sudo crontab -l ; echo "*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log")| sudo crontab -
