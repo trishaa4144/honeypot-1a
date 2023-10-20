6bb8810bb5ee005fe889c1f87fb2a9e836ee9fc5
julzhou121 <145222988+julzhou121@users.noreply.github.com>
2023-10-20 09:56:39 -0400

    updated crontab script to match the local host

--- crontab_script.sh
+++ crontab_script.sh
@@ -4,9 +4,7 @@
 # reboot to make sure our baseline firewall rules will be automatically re-applied after full reboot 
 # of the host. Lastly, we will run the monitor_all.sh script every 15 minutes to monitor the states of 
 # our honeypots.  
+@reboot /home/student/honeypot-1a/reboot_script.sh
+*/5 * * * * /home/student/honeypot-1a/recycling_scripts/flagged_check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log
 
-*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log
-@reboot sh /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
-@reboot /home/student/honeypot-1a/recycling_scripts/check_container.sh
-@reboot systemctl start netdata
-*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log
+# */15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log
