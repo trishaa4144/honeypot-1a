d8c7b4f50824cf820830c6936d4fe49a21bd6f01
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-20 03:00:18 -0400

    Created another script for flagged check

--- recycling_scripts/check_container.sh
+++ recycling_scripts/check_container.sh
@@ -6,35 +6,34 @@
 # recycling_script script within it in order to check each honeypot to see if the honeypot 
 # should be recycled (deleted and reconfigured) if the duration of time randomly chosen has passed. 
 
-if [[ -e /tmp/reboot_flag ]]
-then
-	# Define container names and IP names. The containers have constant
-	# names server1, server2, server3, server4, and IPs ip1, ip2, ip3, ip4
-	# for the purpose of this implementation. However, when deploying our 
-	# honeypots, we can easily modify these two lines with correct names. 
 
-	containers=("server1" "server2" "server3" "server4")
-	ips=("128.8.238.194" "128.8.238.101" "128.8.238.173" "128.8.238.212")
-	ports=("1297" "5420" "3956" "2145")
+# Define container names and IP names. The containers have constant
+# names server1, server2, server3, server4, and IPs ip1, ip2, ip3, ip4
+# for the purpose of this implementation. However, when deploying our 
+# honeypots, we can easily modify these two lines with correct names. 
+
+containers=("server1" "server2" "server3" "server4")
+ips=("128.8.238.194" "128.8.238.101" "128.8.238.173" "128.8.238.212")
+ports=("1297" "5420" "3956" "2145")
 
 
-	# Iterate through container/ip indices and call recycling script on
-	# each container-ip pair. Pass in a random value between 45 - 60 minutes.
-	# This value will be used to randomly assign the next time the container
-	# restarts, in a randomized way.
+# Iterate through container/ip indices and call recycling script on
+# each container-ip pair. Pass in a random value between 45 - 60 minutes.
+# This value will be used to randomly assign the next time the container
+# restarts, in a randomized way.
 
-	# Obtain random minute count between 45-60 minutes
-	minutes=$((RANDOM % 16 + 45))
+# Obtain random minute count between 45-60 minutes
+minutes=$((RANDOM % 16 + 45))
 
-	for index in {0..3}; do
-		# Grab container name, ip address from arrays
-		container="${containers[$index]}"
-		ip_address="${ips[$index]}"
-		port_num="${ports[$index]}"
+for index in {0..3}; do
+	# Grab container name, ip address from arrays
+	container="${containers[$index]}"
+	ip_address="${ips[$index]}"
+	port_num="${ports[$index]}"
 
-		# Call recycling script on respective container
-		/home/student/honeypot-1a/recycling_scripts/recycling_script.sh "$minutes" "$ip_address" "$container" "$port_num"
-		sleep 30
-	done
-fi
+	# Call recycling script on respective container
+	/home/student/honeypot-1a/recycling_scripts/recycling_script.sh "$minutes" "$ip_address" "$container" "$port_num"
+	sleep 30
+done
+
 
---
+++ recycling_scripts/flagged_check_container.sh
@@ -0,0 +1,6 @@
+
+
+if [[ -e /tmp/reboot_flag ]]
+then
+    /home/student/honeypot-1a/recycling_scripts/check_container.sh
+fi
