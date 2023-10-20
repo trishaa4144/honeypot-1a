95d9f3b9d8a7959cd91e28ad8522f48b92da850b
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-20 02:32:26 -0400

    deleting the containers at reboot

--- reboot_script.sh
+++ reboot_script.sh
@@ -1,3 +1,4 @@
+/home/student/recycling_scripts/delete_container.sh
 sh /home/student/honeypot-1a/recycling_scripts/firewall_rules.sh
 sleep 5
 systemctl start netdata
--- recycling_scripts/delete_container.sh
+++ recycling_scripts/delete_container.sh
@@ -6,16 +6,16 @@
 # recycling_script script within it in order to check each honeypot to see if the honeypot 
 # should be recycled (deleted and reconfigured) if the duration of time randomly chosen has passed. 
 
-if [[ -e /tmp/reboot_flag ]]
-then
+#if [[ -e /tmp/reboot_flag ]]
+#then
 	# Define container names and IP names. The containers have constant
 	# names server1, server2, server3, server4, and IPs ip1, ip2, ip3, ip4
 	# for the purpose of this implementation. However, when deploying our 
 	# honeypots, we can easily modify these two lines with correct names. 
 
 	containers=("server1" "server2" "server3" "server4")
-	ips=("128.8.238.194" "128.8.238.101" "128.8.238.173" "128.8.238.212")
-	ports=("1297" "5420" "3956" "2145")
+	#ips=("128.8.238.194" "128.8.238.101" "128.8.238.173" "128.8.238.212")
+	#ports=("1297" "5420" "3956" "2145")
 
 
 	# Iterate through container/ip indices and call recycling script on
@@ -24,17 +24,21 @@ then
 	# restarts, in a randomized way.
 
 	# Obtain random minute count between 45-60 minutes
-	minutes=$((RANDOM % 16 + 45))
+	#minutes=$((RANDOM % 16 + 45))
 
 	for index in {0..3}; do
 		# Grab container name, ip address from arrays
 		container="${containers[$index]}"
-		ip_address="${ips[$index]}"
-		port_num="${ports[$index]}"
+		#ip_address="${ips[$index]}"
+		#port_num="${ports[$index]}"
 
 		# Call recycling script on respective container
-		/home/student/honeypot-1a/recycling_scripts/recycling_script.sh "$minutes" "$ip_address" "$container" "$port_num"
-		sleep 30
+		#/home/student/honeypot-1a/recycling_scripts/recycling_script.sh "$minutes" "$ip_address" "$container" "$port_num"
+		#sleep 30
+		sudo lxc-destroy -n $container
 	done
-fi
+
+	rm /home/student/hpotinfo/*
+#fi	
+#fi	
 
