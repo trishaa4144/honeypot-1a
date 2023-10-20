c1068272e38e71d875969c0e320b94179a264361
julzhou121 <145222988+julzhou121@users.noreply.github.com>
2023-10-20 09:58:20 -0400

    switched order of the PermitRootLogin and starting the sshd server so that changes can come into effect

--- recycling_scripts/customize_honeypot.sh
+++ recycling_scripts/customize_honeypot.sh
@@ -60,7 +60,8 @@ esac
 # Install the openssh-server package in the container
 sudo lxc-attach -n "$container_name" -- bash -c 'sudo apt-get install -y openssh-server'
 sleep 25
-sudo lxc-attach -n "$container_name" -- bash -c 'sudo systemctl start ssh'
-sleep 10
 # Permit Root Login
 sudo lxc-attach -n $container_name -- bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config'
+# Start the ssh server
+sudo lxc-attach -n "$container_name" -- bash -c 'sudo systemctl start sshd'
+sleep 10
