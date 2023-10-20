6f4a9882878355715c5278bdcc1b22778a95a6b9
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-20 02:06:39 -0400

    Edit those two scripts to fix crontab problem

--- reboot_script.sh
+++ reboot_script.sh
@@ -6,3 +6,5 @@ sleep 5
 sleep 5
 (sudo crontab -l ; echo "*/5 * * * * /home/student/honeypot-1a/recycling_scripts/check_container.sh >> /home/student/check_logs/check_container_$(date --iso-8601=seconds).log")| sudo crontab -
 (sudo crontab -l ; echo "*/15 * * * * /home/student/honeypot-1a/recycling_scripts/monitor_all.sh >> /home/student/monitor_logs/monitor_$(date --iso-8601=seconds).log")| sudo crontab -
+
+touch /tmp/reboot_flag
