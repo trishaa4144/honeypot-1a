832801a7db3cf1dfcca2796000013ebc24363b89
Trisha <77016955+trishaa4144@users.noreply.github.com>
2023-10-27 18:54:13 -0400

    update idle time check logic

--- recycling_scripts/check_attacker_exit_idle.sh
+++ recycling_scripts/check_attacker_exit_idle.sh
@@ -20,21 +20,28 @@ if [[ $# -ne 1 ]]; then
 fi
 
 container_name=$1
-echo "$(date --iso-8601=seconds): MITM check - Checking MITM log attackers in $container_name." >> /home/student/check_logs/recycling_debug.log
+echo "$(date --iso-8601=seconds): MITM check - Checking MITM log for attackers in $container_name." >> /home/student/check_logs/recycling_debug.log
 
 if [[ -e /home/student/hpotinfo/mitm_location_$container_name ]]; then
     mitm_location=$(cat /home/student/hpotinfo/mitm_location_$container_name)
+    time_line=$(grep "Auto-access is now disabled" "$mitm_location")    
 
     # If auto access has been disabled at some point in the file, update the time file to
     # be the current time + 2 minutes (since this is checked every 2 minutes, effectively
     # kick out any attackers after 2-4 minutes, but provide enough grace period for
     # them to perform an attack.)
-    if grep -q "Auto-access is now disabled" "$mitm_location"; then
-        echo "$(date --iso-8601=seconds): MITM check - Auto-access was disabled on $container_name, updating its recycle time." >> /home/student/check_logs/recycling_debug.log
+    if [[ -n $time_line ]]; then
+        time=$(echo "$time_line" | awk '{print $1, $2}')
+        time_unix=$(date -d "$time" "+%s")
         curr_time=$(date +"%s")
         seconds=$((2 * 60))
-        goal_time=$((curr_time + seconds))
-        echo "$container_name $goal_time" > /home/student/hpotinfo/time_$container_name
+        threshold_time=$((curr_time - seconds))
+        if [[ $time_unix -le $threshold_time ]]; then
+            echo "$(date --iso-8601=seconds): MITM check - Auto-access was disabled on $container_name, updating its recycle time." >> /home/student/check_logs/recycling_debug.log
+            echo "$container_name $curr_time" > /home/student/hpotinfo/time_$container_name
+        else
+            echo "$(date --iso-8601=seconds): MITM check - Auto-access was less than 2 minutes ago on $container_name, no need to recycle yet." >> /home/student/check_logs/recycling_debug.log
+        fi
     else
         echo "$(date --iso-8601=seconds): MITM check - Checked $mitm_location, No attacker has connected yet to $container_name" >> /home/student/check_logs/recycling_debug.log
     fi
