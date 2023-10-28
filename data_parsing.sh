dcc15c97310fb77e857d128da241ac89a19f0f18
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2023-10-28 18:26:46 -0400

    Added command to get total number of unique IPs

--- data_parsing.sh
+++ data_parsing.sh
@@ -7,7 +7,7 @@ fi
 
 language=$1
 current_date=$(date --iso-8601)
-# total_unique_ips=$() # ADD THE COMMAND HERE
+total_unique_ips=$(cat * | grep "Attacker connected" | cut -d' ' -f8 | sort | uniq | wc -l) # ADD THE COMMAND HERE
 
 if [[ ! -d /home/student/data/ ]]; then
     mkdir /home/student/data/
@@ -26,6 +26,8 @@ cd /home/student/mitm_logs/$language
 
 # gets number of unique IP addresses of attackers & counts how many times each appears
 cat * | grep "Attacker connected" | cut -d " " -f8 | sort | uniq -c | sort -nr > /home/student/data/$language/$current_date/ips.file
+
+echo $total_unique_ips > /home/student/data/$language/$current_date/ips.file
 
 # gets all unique interactive commands entered by the attacker (and the number of times they were used)
 cat * | grep -w "line from reader" | cut -d " " -f9- | sort | uniq -c | sort -rn > /home/student/data/$language/$current_date/interactive.file
