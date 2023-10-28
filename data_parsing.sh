b4114f1ae24978afc5d73bcbcc9b86544e9f3f4c
julzhou121 <145222988+julzhou121@users.noreply.github.com>
2023-10-27 21:48:16 -0400

    updated parsing file so that each file will be stored in their respective folders

--- data_parsing.sh
+++ data_parsing.sh
@@ -17,15 +17,19 @@ if [[ ! -d /home/student/data/$language ]]; then
     mkdir /home/student/data/$language
 fi
 
+if [[ ! -d /home/student/data/$language/$current_date ]]; then
+    mkdir /home/student/data/$language/$current_date
+fi
+
 # go into the mitm_logs folder of the particular language we want to assess
 cd /home/student/mitm_logs/$language
 
 # gets number of unique IP addresses of attackers & counts how many times each appears
-cat * | grep "Attacker connected" | cut -d " " -f8 | sort | uniq -c | sort -nr > /home/student/data/$language/ips_$current_date.file
+cat * | grep "Attacker connected" | cut -d " " -f8 | sort | uniq -c | sort -nr > /home/student/data/$language/$current_date/ips.file
 
 # gets all unique interactive commands entered by the attacker (and the number of times they were used)
-cat * | grep -w "line from reader" | cut -d " " -f9- | sort | uniq -c | sort -rn > /home/student/data/$language/interactive_$current_date.file
+cat * | grep -w "line from reader" | cut -d " " -f9- | sort | uniq -c | sort -rn > /home/student/data/$language/$current_date/interactive.file
 
 # gets all unique noninteractive commands entered by the attacker (and the number of times they were used)
-cat * | grep -w "Noninteractive" | cut -d " " -f10- | sort | uniq -c | sort -rn > /home/student/data/$language/noninteractive_$current_date.file
+cat * | grep -w "Noninteractive" | cut -d " " -f10- | sort | uniq -c | sort -rn > /home/student/data/$language/$current_date/noninteractive.file
 
