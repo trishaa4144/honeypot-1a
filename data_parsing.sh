2b5d85b9cbb08eaf22ebd07a6745bec54d5a6d00
julzhou121 <145222988+julzhou121@users.noreply.github.com>
2023-10-28 22:19:21 -0400

    got rid of vars bc they might be messing up formatting of data files

--- data_parsing.sh
+++ data_parsing.sh
@@ -26,21 +26,18 @@ cd /home/student/mitm_logs/$language
 
 echo "Total number of unique IPs: $total_unique_ips" >> /home/student/data/$language/$current_date/ips.file
 # gets number of unique IP addresses of attackers & counts how many times each appears on the current date
-unique_ips=$(cat *$current_date* | grep "Attacker connected" | cut -d " " -f8 | sort | uniq -c | sort -nr)
-echo $unique_ips >> /home/student/data/$language/$current_date/ips.file
+cat *$current_date* | grep "Attacker connected" | cut -d " " -f8 | sort | uniq -c | sort -nr >> /home/student/data/$language/$current_date/ips.file
 
 # gets the timestamp & interactive command entered in all sessions on the current date
-interactive=$(cat *$current_date* | grep -w "line from user" | colrm 1 11 | colrm 14 49)
-echo $interactive >> /home/student/data/$language/$current_date/interactive.file
+cat *$current_date* | grep -w "line from user" | colrm 1 11 | colrm 14 49 >> /home/student/data/$language/$current_date/interactive.file
 
 # gets the timestamp & noninteractive command entered in all sessions on the current date
-noninteractive=$(cat *$current_date* | grep -w "Noninteractive" | colrm 1 11 | colrm 14 68)
-echo $noninteractive >> /home/student/data/$language/$current_date/noninteractive.file
+cat *$current_date* | grep -w "Noninteractive" | colrm 1 11 | colrm 14 68 >> /home/student/data/$language/$current_date/noninteractive.file
 
 # finally, gets the # of sessions & # of successful logins & puts them in the same file
 logins=$(cat *"$current_date"* | grep -w "LXC-Auth" | wc -l)
 sessions=$(ls *"$current_date"* | wc -l)
 
 echo "Number of successful logins: $logins" >> /home/student/data/$language/$current_date/session_logins.file
-echo "Number of sessions on $date: $sessions" >> /home/student/data/$language/$current_date/session_logins.file
+echo "Number of sessions on $current_date: $sessions" >> /home/student/data/$language/$current_date/session_logins.file
 
